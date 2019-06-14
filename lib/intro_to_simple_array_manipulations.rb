f6774897fff40a6348d25adc093394540dbe29b7
John Odom <joodom91@gmail.com>
2019-06-14 15:55:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -20,5 +20,8 @@ def using_concat(array1, array2)
   array1 = array1 + array2
 end
 def using_insert(array, element)
-array = array.insert(3, element)
+array = array.insert(4, element)
+end
+def using_uniq(array)
+  
 end
